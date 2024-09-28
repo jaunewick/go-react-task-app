942090a7368d119ec22b42cc6c89f1356b35773d
Daniel Giao <daniel.giao@icloud.com>
2024-09-28 01:33:52 -0400

    Setup go http server with fiber and air

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+    fmt.Println("Server is running...")
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.Status(200).JSON(fiber.Map{"data": "Hello World"})
+    })
+    log.Fatal(app.Listen(":4000"))
+}
