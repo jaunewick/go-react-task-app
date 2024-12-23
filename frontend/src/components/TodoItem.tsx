dcc7ee88c8a23ba79ef2dca090076ec91da022d1
Daniel Giao <daniel.giao@icloud.com>
2024-12-22 22:40:05 -0500

    Added delete a todo on frontend

--- frontend/src/components/TodoItem.tsx
+++ frontend/src/components/TodoItem.tsx
@@ -7,6 +7,7 @@ import { BASE_URL } from "../App";
 
 const TodoItem = ({ todo }: { todo: Todo }) => {
     const queryClient = useQueryClient();
+
     const {mutate:updateTodo, isPending:isUpdating} = useMutation({
         mutationKey:["updateTodo"],
         mutationFn: async () => {
@@ -28,6 +29,28 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
             queryClient.invalidateQueries({queryKey:["todos"]});
         }
     })
+
+    const {mutate:deleteTodo, isPending:isDeleting} = useMutation({
+        mutationKey:["deleteTodo"],
+        mutationFn: async () => {
+            try {
+                const res = await fetch(BASE_URL + `/todos/${todo._id}`, {
+                    method: "DELETE",
+                })
+                const data = await res.json();
+                if (!res.ok) {
+                    throw new Error(data.error || "Something went wrong!");
+                }
+                return data;
+            } catch (error) {
+                console.error(error);
+            }
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey:["todos"]});
+        }
+    })
+
     return (
         <Flex gap={2} alignItems={"center"}>
             <Flex
@@ -60,8 +83,8 @@ const TodoItem = ({ todo }: { todo: Todo }) => {
                 <Box color={"green.500"} cursor={"pointer"} onClick={() => updateTodo()}>
                     {!isUpdating ? <FaCheckCircle size={20} /> : <Spinner size={"sm"} />}
                 </Box>
-                <Box color={"red.500"} cursor={"pointer"}>
-                    <MdDelete size={25} />
+                <Box color={"red.500"} cursor={"pointer"} onClick={() => deleteTodo()}>
+                    {!isDeleting ? <MdDelete size={25} />: <Spinner size={"sm"} />}
                 </Box>
             </Flex>
         </Flex>
